c319a4ffdad1a6e7363daed45d1426d00cc9e6cc
wackopastorius <ladaegolaeva@gmail.com>
2022-10-17 11:39:10 +0200

    added comment in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        /** försökte skapa ett objekt av
+         * klassen ContestContributor här och anropa metoden displayArray()
+         * genom punktnotation men det går inte den här gången då objektet kräver arguments.
+         */
 
     }
 }
